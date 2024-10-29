a51c9148e0f7d31e4620d12e770fca43f379d44d
katie-ucd <katieaflynn2@gmail.com>
2024-10-29 12:17:43 +0000

    updated url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://Katie-UCD/astro1.github.io',
+  site: 'https://Katie-UCD.github.io',
   base: 'my-repo',
 })
