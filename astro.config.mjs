4f657056309b63b207249e83b9d3099b9d64b554
katie-ucd <katieaflynn2@gmail.com>
2024-10-29 10:47:37 +0000

    new url trial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://astro1.github.io',
+  site: 'https://Katie-UCD/astro1.github.io',
   base: '/astro1',
 })
